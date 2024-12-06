f22994047ab6a1e039fd2597f79960856d28b827
Ratovonirina <eric.ratovonirina@gmail.com>
2024-12-06 11:49:53 +0300

    main : implement department crud

--- src/department/department.controller.ts
+++ src/department/department.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
+import { DepartmentService } from './department.service';
+import { CreateDepartmentDto } from './dto/create.department.dto';
 
 @Controller('department')
-export class DepartmentController {}
+export class DepartmentController {
+  constructor(private departmentService: DepartmentService) {}
+
+  @Get('all')
+  getAll() {
+    return this.departmentService.getAll();
+  }
+
+  @Get('one')
+  getOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.departmentService.getOne(id);
+  }
+
+  @Post('create')
+  create(@Body() data: CreateDepartmentDto) {
+    return this.departmentService.create(data);
+  }
+
+  @Post('update/:id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() data: CreateDepartmentDto,
+  ) {
+    return this.departmentService.update(id, data);
+  }
+
+  @Post('delete/:id')
+  delete(@Param('id', ParseUUIDPipe) id: string) {
+    return this.departmentService.delete(id);
+  }
+}
--- src/department/department.service.ts
+++ src/department/department.service.ts
@@ -1,4 +1,36 @@
+import { CreateDepartmentDto } from './dto/create.department.dto';
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { UpdateDepartmentDto } from './dto/update.department.dto';
 
 @Injectable()
-export class DepartmentService {}
+export class DepartmentService {
+  constructor(private prismaService: PrismaService) {}
+
+  async getAll() {
+    return this.prismaService.department.findMany();
+  }
+
+  async getOne(id: string) {
+    return this.prismaService.department.findUnique({
+      where: { id },
+    });
+  }
+
+  async create(data: CreateDepartmentDto) {
+    return this.prismaService.department.create({ data });
+  }
+
+  async update(id: string, data: UpdateDepartmentDto) {
+    return this.prismaService.department.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    return this.prismaService.department.delete({
+      where: { id },
+    });
+  }
+}
